e9c11f04e12f0ee9e749b9892d4164b7d122b015
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2022-04-01 18:44:23 +0530

    Update Zetwork1.js

--- Interview/Zetwork1.js
+++ Interview/Zetwork1.js
@@ -1,16 +1,16 @@
 // Online Javascript Editor for free
 // Write, Edit and Run your Javascript code using JS Online Compiler
 
-let a=[10,20,30]
+let a=[10,20,30] 
 let b = a
 b[1]=40
-console.log(a)
-console.log(b)
+console.log(a) //[10,40,30]
+console.log(b) //[10,40,30]
 
 const a1 = {name:{title:'yash'}}
 const b1 = a1
 b1.name='ram'
-console.log(a1)
-console.log(b1)
+console.log(a1) //{name:'ram'}
+console.log(b1) //{name:'ram'}
 
 
