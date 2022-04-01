d4fca32d475b5abda357816ff4edbc3675d2efde
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2022-04-01 18:54:34 +0530

    Update Zetwork1.js

--- Interview/Zetwork1.js
+++ Interview/Zetwork1.js
@@ -13,4 +13,17 @@ b1.name='ram'
 console.log(a1) //{name:'ram'}
 console.log(b1) //{name:'ram'}
 
+-----------------------------------------------
+  
+let a=[10,20,30]
+let b = [...a];
+b[1]=40
+console.log(a)
+console.log(b)
+
+const a1 = {name:{title:'yash'}}
+const b1 = {...a1}
+b1.name='ram'
+console.log(a1)
+console.log(b1)
 
