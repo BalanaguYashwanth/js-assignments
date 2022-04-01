6a38c1f048f167858451cb7f82e505cf330b8fba
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2022-04-01 18:43:10 +0530

    Create Zetwork1.js

---
+++ Interview/Zetwork1.js
@@ -0,0 +1,16 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+let a=[10,20,30]
+let b = a
+b[1]=40
+console.log(a)
+console.log(b)
+
+const a1 = {name:{title:'yash'}}
+const b1 = a1
+b1.name='ram'
+console.log(a1)
+console.log(b1)
+
+
