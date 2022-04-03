8bc583dedf3af00b3deac789d9317a460f8bba90
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2022-04-03 13:21:53 +0530

    Create Zetwork4.js

---
+++ Interview/Zetwork4.js
@@ -0,0 +1,15 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+var name='yash'
+
+let data={
+    a:name;
+    newdata:function(){
+        var name='bash'
+        console.log(name)
+    }
+}
+console.log(data['a']);
+
+
