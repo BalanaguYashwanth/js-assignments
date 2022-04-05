6fc6b5b9732a78bbc19112ca95def03a3becf049
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2022-04-05 13:53:15 +0530

    Create Problem2.js

---
+++ Interview/Minddeft/Problem2.js
@@ -0,0 +1,10 @@
+/*
+   arr=[3,5,2,7,9,1,8,4] 
+   
+   Elements are represented as students with their height numbers in array, so some students are not able to see the other students due to inbetween the left and right 
+   person height is more
+   
+   count those students and print the total count.
+   
+   here count is 2 which are element 2nd element and 1st element.
+*/
